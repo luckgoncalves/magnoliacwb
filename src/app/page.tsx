ca1a8b91c2b47c9de172611f9e765d1cd7df00ed
Lucas Gonçalves <lucasleitegoncalves@gmail.com>
2023-05-09 19:54:00 -0300

    feat: add image woman

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,23 @@
 import Image from 'next/image'
-import { Metadata } from 'next';
-
- 
-export const metadata: Metadata = {
-  title: 'Office Beauty',
-  description: 'Salão de beleza em Curitiba, especializado em cabelos, unhas, sobrancelhas, cílios, piercing.',
-  icons: {
-    icon: '/images/logo-ob.png',
-  }
-};
-
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-r from-white to-[#FFDAD2] p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-center font-mono text-sm ">
-        <h1 className="fixed text-3xl left-0 top-0 flex w-full font-serif font-semibold text-[#E79C88] justify-center pb-6 pt-8 backdrop-blur-2xl bg-gradient-to-r from-white to-[#FFDAD2]">
-          OFFICE BEAUTY
-        </h1>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-r from-white to-[#FFDAD2]">
-          <a
-            className="pointer-events-none flex place-items-center text-[#E79C88] gap-2 p-8"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              src="/images/logo-ob.png"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+      <div className=" w-full flex flex-col-reverse md:flex-row justify-around items-center text-center font-semibold text-[#E79C88]">
+        <div>
+          <p className={`m-0 max-w-[30ch] text-center `}>
+            Rua Emanuel Kant, 60, Sala 418 <br />Capão Raso, Curitiba - PR, 81020-670
+          </p>        
+          <a href="https://api.whatsapp.com/send/?phone=5541988245295&text&type=phone_number&app_absent=0" target='_blank'>(41) 98824-5295</a>
         </div>
-      </div>
-
-      <div className="text-center font-semibold text-[#E79C88]">
-        <p className={`m-0 max-w-[30ch] text-center `}>
-          Rua Emanuel Kant, 60 - Capão Raso, Curitiba - PR, 81020-670
-        </p>        
-        <a href="https://api.whatsapp.com/send/?phone=5541988245295&text&type=phone_number&app_absent=0" target='_blank'>(41) 98824-5295</a>
+        <div>
+          <Image
+            src="/images/womans-face-line-art.svg"
+            alt="Woman"
+            width={300}
+            height={300}
+            priority
+          />
+        </div>
       </div>
     </main>
   )
